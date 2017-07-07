cea40d32b96c4a9d2229c70033b721523174bff3
Jack Neff <neffj@flip1.engr.oregonstate.edu>
2017-07-07 13:16:04 -0700

    Prints out result and exectuion time-- careful! large inputs will run for a very long time

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cpp_greatest_palindrome
 Finds largest palindrome product of two numers with n digits each
+
+Input: A number n
+
+Output: 1) Largest palindrome product of two n-digit numbers
+	2) Time (ms) for program execution
+  
--- gp.cpp
+++ gp.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <typeinfo>
 #include <cmath>
+#include <ctime>
+#include <algorithm>
+
 
 int get_length(int r) {
 	int length = 0;
@@ -13,12 +15,12 @@ int get_length(int r) {
 
 int check_if_palindrome(int original){
 	int modified = original;		//preserve s, which will be modified
-	int length = get_length(original);		
+	int length = get_length(original); 
 	int fwd[length];
 	int bwd[length];
 	for(int k = 0; k < length; k++){ 
 		fwd[k] = modified % 10;
-		bwd[(length-k-1)] = modified % 10;
+		bwd[(length-k-1)] = fwd[k];
 		modified /= 10;
 	}
 	for(int l = 0; l < length; l++){ 
@@ -30,16 +32,28 @@ int check_if_palindrome(int original){
 } 
 
 int main(int argc, char* argv[]){
-	int input_a, input_b, length_of_a, product_d;
-	std::cin >> input_a >> input_b;
-	length_of_a = get_length(input_a); 
-	for (int i = pow(10, (length_of_a - 1)); i <= pow(10, length_of_a)-1; i++){
-		for (int j = 0; j <= i; j++){
+	int input_a; 
+	int product_d;
+	int greatest_palindrome = 0;
+	double time;
+	clock_t start = clock();
+ 	std::cin >> input_a;
+	int lower_bound = pow(10, (input_a - 1));
+	int upper_bound = pow(10, input_a);
+	for (int i = lower_bound; i < upper_bound; i++){
+		for (int j = lower_bound; j < i; j++){
 			product_d = check_if_palindrome(i*j);   
 			if(product_d != 0){
  				std::cout << i << " " << j << ": ";
-				std::cout << product_d << std::endl; 
+				std::cout << product_d << std::endl;
+				if(greatest_palindrome < product_d){
+					greatest_palindrome = product_d;
+				}	  
 			}		
 		}
 	}
-}
+	std::cout << "The largest palindrome number produced by multiplyling two " << input_a << " digit numbers is " << greatest_palindrome << std::endl;
+	clock_t end = clock();
+	time = (end - start)/1000;
+	std::cout << "time: " << time << std::endl;
+}	
